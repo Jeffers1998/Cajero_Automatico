0d0a510c765dc4f2aaff4a6fbad540f99f704add
cadri2001 <carlos.coloma@epn.edu.ec>
2021-11-13 18:38:53 -0500

    Se escribio el codigo del metodo iniciar

--- CajeroAutomatico/src/CajeroAutomatico.java
+++ CajeroAutomatico/src/CajeroAutomatico.java
@@ -7,16 +7,44 @@ public class CajeroAutomatico {
 	private DispensadorDinero dispensador;
 	
 	private BaseDeDatos baseDatos;
-	
+	private int numeroCuentaActual;
 	public CajeroAutomatico() {
+		usuarioAutenticado = false;
+		
 		pantalla = Pantalla.getInstance();
 		teclado = Teclado.getInstance();
 		dispensador = DispensadorDinero.getInstance();
 		
 		baseDatos = BaseDeDatos.getInstance();
 	}
+	
 	public void iniciar() {
-		
+		while(!usuarioAutenticado)
+			numeroCuentaActual = solicitarAutenticacion();
+			
+		while(true) {
+			int transaccionEscogida = solicitarTransaccion();
+			if(!seEscogioSalir(transaccionEscogida)) {
+				Transaccion transaccionActual = Transaccion.crearTransaccion(transaccionEscogida, numeroCuentaActual);
+				transaccionActual.realizar();
+			}else
+				break;
+		}
+	}
+	
+	private boolean seEscogioSalir(int transaccionEscogida) {
+		//TODO
+		return false;
+	}
+	
+	private int  solicitarAutenticacion() {
+		//TODO
+		return 0;
+	}
+	
+	private int solicitarTransaccion() {
+		//TODO
+		return 0;
 	}
 	
 }
