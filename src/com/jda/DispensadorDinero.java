731018e286c6dc170b3cabb922e371a68e2ee5ce
cadri2001 <carlos.coloma@epn.edu.ec>
2021-11-17 20:24:28 -0500

    Se creo otra vez con el nombre de la carpeta correcto

---
+++ src/com/jda/DispensadorDinero.java
@@ -0,0 +1,30 @@
+package com.jda;
+
+public class DispensadorDinero {
+	private int cantidadDinero;
+	
+	private static DispensadorDinero instance = new DispensadorDinero();
+	private DispensadorDinero() {
+		cantidadDinero = 500; //Cantidad inicial
+	}
+	
+	public static DispensadorDinero getInstance() {
+		return instance;
+	}
+	/**
+	 * 
+	 * @param cantidad La cantidad de dinero a expulsar
+	 */
+	public void expulsarDinero(double cantidad) {
+		cantidadDinero -= cantidad;
+	}
+	
+	/**
+	 * 
+	 * @param cantidad La cantidad de dinero que se quiere retirar
+	 * @return true si es que hay suficiente dinero y false en caso contrario
+	 */
+	public boolean haySuficienteDineroDisponible(int cantidad) {
+		return cantidad <= cantidadDinero;
+	}
+}
---
+++ src/com/jda/Retiro.java
@@ -0,0 +1,55 @@
+package com.jda;
+
+public class Retiro extends Transaccion{
+
+	private DispensadorDinero dispensador;
+	private Teclado teclado;
+	private Pantalla pantalla;
+	private BaseDeDatos baseDatos;
+	
+	private int retiro1 = 10;
+	private int retiro2 = 25;
+	private int retiro3 = 65;
+	private int retiro4 = 100;
+	
+	public Retiro(int numeroCuenta) {
+		super(numeroCuenta);
+		dispensador = DispensadorDinero.getInstance();
+		teclado = Teclado.getInstance();
+		pantalla = Pantalla.getInstance();
+		baseDatos = BaseDeDatos.getInstance();
+		baseDatos = null;
+	}
+
+	@Override
+	public void realizar() {
+		pantalla.mostrarMensaje("Seleccionar Monto \n"
+				+ " a. " + retiro1 + "\n"
+				+ " b. " + retiro2 + "\n"
+				+ " c. " + retiro3 + "\n"
+				+ " d. " + retiro4 + "\n");
+		double cantidad = 0;
+		switch(teclado.getEntrada()) {
+		case "a":
+			cantidad = retiro1;
+			break;
+		case "b":
+			cantidad = retiro2;
+			break;
+		case "c":
+			cantidad = retiro3;
+			break;
+		case "d":
+			cantidad = retiro4;
+			break;
+		}
+		
+		baseDatos.retirarDinero(numeroCuenta, cantidad);
+		dispensador.expulsarDinero(cantidad);
+		pantalla.mostrarMensaje("Retiro Exitoso: \n"
+				+ "Id Cuenta: " + numeroCuenta + "\n"
+				+ "Retiro: " + cantidad + "\n"
+				+ "Actual: " + baseDatos.getDineroDisponible(numeroCuenta));
+	}
+
+}
