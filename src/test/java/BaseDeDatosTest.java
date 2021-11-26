199e4d30512d484ae83228193882981bcc44109d
cadri2001 <carlos.coloma@epn.edu.ec>
2021-11-26 14:14:18 -0500

    Test de prueba

---
+++ src/test/java/BaseDeDatosTest.java
@@ -0,0 +1,17 @@
+import com.jda.CajeroAutomatico;
+import com.jda.Cuenta;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+
+public class BaseDeDatosTest {
+
+    @org.junit.Test
+    public void getDineroDisponible() {
+        Cuenta cuenta = Mockito.mock(Cuenta.class);
+    }
+
+    @org.junit.Test
+    public void getSaldoTotal() {
+    }
+}
